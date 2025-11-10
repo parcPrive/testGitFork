c2b836a35831246385f1225d00d26441fad155a6
chingudl7 <chingudl7@naver.com>
2025-11-10 19:06:22 +0900

    test fork

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-4">hihihihihihihi</h1>
+      <h1 className="text-center mb-4">배고파요..</h1>
     </div>
   );
 }
